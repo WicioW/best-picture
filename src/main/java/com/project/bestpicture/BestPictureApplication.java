740d8295c2941e60b357558de3bd68a0d645647b
wiktor <wwolakksiazek@fream.pl>
2022-06-19 14:46:03 +0200

    Starting project

---
+++ src/main/java/com/project/bestpicture/BestPictureApplication.java
@@ -0,0 +1,13 @@
+package com.project.bestpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestPictureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/bestpicture/BestPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bestpicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
